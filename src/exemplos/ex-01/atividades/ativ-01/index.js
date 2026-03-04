7cabfa9ec44580793aeb6ce9d0172d440e980ac5
LucasLima26 <lukaslimah113@gmail.com>
2026-03-03 22:46:39 -0300

    Atividade 1

---
+++ src/exemplos/ex-01/atividades/ativ-01/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+function Atividade01 () {
+    return(
+        <View  style ={styles.container} >
+            <Text style={styles.texto2} >aprendendo react 1</Text>
+        </View>
+    );
+}
+
+export default Atividade01;
---
+++ src/exemplos/ex-01/atividades/ativ-01/styles.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container : {
+        backgroundColor: '#ffff00' ,
+        // margin: 20, 
+        padding: 20,
+        flex: 1,
+        borderRadius: 20,
+    },
+     titulo: {
+        fontSize: 30,
+        width: '100%',
+        textAlign: 'center',
+        fontWeight: 'bold',
+       
+    },
+
+    texto: {
+        fontSize: 18,
+    },
+    texto2: {
+        fontSize: 22,
+        color: '#00f',
+        fontFamily: 'Arial',
+        textAlign: 'center',
+         
+
+
+    }
+});
+export default styles;
