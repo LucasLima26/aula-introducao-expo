1181aa91d01b59ca4665f5ea2b1c8a368d849978
LucasLima26 <lukaslimah113@gmail.com>
2026-04-14 22:40:53 -0300

    atividade calculadora 14-04

--- App.js
+++ App.js
@@ -5,17 +5,22 @@ import Exemplo01 from './src/exemplos/ex-01';
 import Exemplo02 from './src/exemplos/ex-02';
 import Exmeplo03 from './src/exemplos/ex03';
 import Exemplo04 from './src/exemplos/ex04';
+import Exemplo05 from './src/exemplos/ex05';
+import Exemplo06 from './src/exemplos/ex06';
 
 import Atividade02 from './src/atividades/atv-02';
 import Atividade03 from './src/atividades/atv-03';
-import Atividade04 from './src/atividades/atv-04'
+import Atividade04 from './src/atividades/atv-04';
+import Atividade05 from './src/atividades/atv-05';
+import Atividade06 from './src/atividades/atv-06'
+
 
 
 export default function App() {
   return (
 
     <View style={styles.container}>
-      <Atividade04/>
+      <Atividade06/>
       <StatusBar style="auto" />
     </View>
   );
